1d502c9e47b78ab9a0073f91e2045579e7bcfc0e
Eric Alves Frota <ericeaf@hotmail.com>
2025-03-01 14:50:47 -0300

    fds

--- src/components/layouts/Navigation/index.jsx
+++ src/components/layouts/Navigation/index.jsx
@@ -9,12 +9,12 @@ const Navegation = () => {
 
   return (
     <>
-      <nav className="justify-between h-16 w-full fixed flex items-center pr-6 pl-6 after:absolute after:-bottom-4 after:left-0 after:w-full after:h-[16px] after:bg-gradient-to-r after:from-5 after:via-2 after:to-5">
+      <header className="justify-between h-16 w-full fixed flex items-center pr-6 pl-6 after:absolute after:-bottom-4 after:left-0 after:w-full after:h-[16px] after:bg-gradient-to-r after:from-5 after:via-2 after:to-5">
         <Logo />
         <Navibar />
         <HamburgerMenu />
         {isNavOppen && <Sidebar />}
-      </nav>
+      </header>
     </>
   );
 };

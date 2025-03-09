989a22811d5335e133fdd440ba758681294fdd17
Eric Alves Frota <ericeaf@hotmail.com>
2025-03-09 16:15:20 -0300

    fix: body size

--- src/components/layouts/Navigation/index.jsx
+++ src/components/layouts/Navigation/index.jsx
@@ -6,7 +6,7 @@ import { motion } from "motion/react";
 const Navegation = () => {
   return (
     <>
-      <nav className="bg-5 justify-between z-10 after:-z-20 h-14 md:h-18 w-full fixed flex items-center pr-6 pl-6 after:absolute after:-bottom-4 after:left-0 after:w-full after:h-[16px] after:bg-gradient-to-r after:from-5 after:via-2 after:to-5">
+      <nav className="bg-5 justify-between z-10 after:-z-20 h-14 md:h-18 w-[100vw] fixed flex flex-wrap items-center pr-6 pl-6 after:absolute after:-bottom-4 after:left-0 after:w-full after:h-[16px] after:bg-gradient-to-r after:from-5 after:via-2 after:to-5">
         <Logo />
         <motion.div
           variants={entryAnimation()}
